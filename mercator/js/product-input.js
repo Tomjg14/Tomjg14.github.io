3b9e64ecd94b0c201d73b71edbdd177b4f06840f
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-07 10:16:38 +0200

    added add button to product input page

--- mercator/js/product-input.js
+++ mercator/js/product-input.js
@@ -1,7 +1,17 @@
 const backBtn = document.getElementById('back');
+const addBtn = document.getElementById('add');
 
 backBtn.addEventListener("click", e =>{
 	e.preventDefault();
 	
 	window.location="product-menu.html";
+});
+
+addBtn.addEventListener("click", e => {
+	var doc = document.implementation.createHTMLDocument();
+	doc.body.append("Hello World!");
+	var iframeDoc = document.querySelector('iframe').contentDocument;
+	iframeDoc.replaceChild(
+	doc.documentElement,
+	iframeDoc.documentElement);
 });
