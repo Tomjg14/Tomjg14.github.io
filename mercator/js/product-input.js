363cc2cee89763d2c85b76f36b2ab1e71fc1865b
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-13 15:56:19 +0200

    removed add functionality in other js file

--- mercator/js/product-input.js
+++ mercator/js/product-input.js
@@ -1,17 +1,7 @@
 const backBtn = document.getElementById('back');
-const addBtn = document.getElementById('add');
 
 backBtn.addEventListener("click", e =>{
 	e.preventDefault();
 	
 	window.location="product-menu.html";
-});
-
-addBtn.addEventListener("click", e => {
-	var doc = document.implementation.createHTMLDocument();
-	doc.body.append("Hello World!");
-	var iframeDoc = document.querySelector('iframe').contentDocument;
-	iframeDoc.replaceChild(
-	doc.documentElement,
-	iframeDoc.documentElement);
 });
