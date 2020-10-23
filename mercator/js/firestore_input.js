e4fc01a513e47eccb50bba8fa886986e8d4f3ee7
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-23 14:00:57 +0200

    first attempt to upload data to firestore

--- mercator/js/firestore_input.js
+++ mercator/js/firestore_input.js
@@ -1,11 +1,5 @@
 const form = document.querySelector('form')
 let fileUpload = document.getElementById('bol_trends')
-
-fileUpload.addEventListener('change', function(evt) {
-	let firstFile = evt.target.files[0]
-	console.log(firstFile);
- })
- 
  
 form.addEventListener('submit', e => {
 	e.preventDefault();
@@ -17,11 +11,30 @@ form.addEventListener('submit', e => {
 	var bol_link = document.querySelector("input[id='bol_link']").value;
 	var gem_verkoopprijs = document.querySelector("input[id='gem_verkoopprijs']").value;
 	var bol_trends = document.querySelector("input[id='bol_trends']").files[0];
-	console.log(bol_trends);
+	var gem_inkoopprijs = document.querySelector("input[id='gem_inkoopprijs']").value;
+	var nr_zoekresultaten = document.querySelector("input[id='nr_zoekresultaten']").value;
+	var toelichting = document.querySelector("input[id='toelichting']").value;
+	var afmetingen = document.querySelector("input[id='afmetingen']").value;
+	var gewicht = document.querySelector("input[id='gewicht']").value;
+	
+	const productCollectionRef = db.collection('mercator-product-review');
 	
 	firebase.auth().onAuthStateChanged(function(user) {
 		if(user) {
 			console.log("user logged in");
+			productCollectionRef.add({
+				product_naam: product_name,
+				bol_link: bol_link,
+				gem_verkoopprijs: gem_verkoopprijs,
+				bol_trends: bol_trends,
+				gem_inkoopprijs: gem_inkoopprijs,
+				nr_zoekresultaten: nr_zoekresultaten,
+				toelichting: toelichting,
+				afmetingen: afmetingen,
+				gewicht: gewicht
+			}).then(function() {
+				console.log("added info");
+			});
 		} else {
 			console.log("user not logged in");
 		}
