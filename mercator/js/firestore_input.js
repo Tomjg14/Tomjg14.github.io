237d69b484f010864bb806fd71540a9a48d817e5
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-23 14:02:32 +0200

    removed dubble ref

--- mercator/js/firestore_input.js
+++ mercator/js/firestore_input.js
@@ -5,8 +5,6 @@ form.addEventListener('submit', e => {
 	e.preventDefault();
 	const db = firebase.firestore();
 	
-	const productCollectionRef = db.collection('products');
-	
 	var product_name = document.querySelector("input[id='product_name']").value;
 	var bol_link = document.querySelector("input[id='bol_link']").value;
 	var gem_verkoopprijs = document.querySelector("input[id='gem_verkoopprijs']").value;
