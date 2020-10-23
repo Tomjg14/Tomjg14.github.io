3ed7203d294ea77e689e59bb3d6b4b8c248f4425
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-23 13:20:17 +0200

    added event listener to image upload

--- mercator/js/firestore_input.js
+++ mercator/js/firestore_input.js
@@ -1,5 +1,10 @@
 const form = document.querySelector('form')
+let fileUpload = document.getElementById('bol_trends')
 
+fileUpload.addEventListener('change', function(evt) {
+	let firstFile = evt.target.files[0]
+	console.log(firstFile);
+ })
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	const db = firebase.firestore();
@@ -7,7 +12,10 @@ form.addEventListener('submit', e => {
 	const productCollectionRef = db.collection('products');
 	
 	var product_name = document.querySelector("input[id='product_name']").value;
-	console.log(product_name);
+	var bol_link = document.querySelector("input[id='bol_link']").value;
+	var gem_verkoopprijs = document.querySelector("input[id='gem_verkoopprijs']").value;
+	
+	
 	
 	firebase.auth().onAuthStateChanged(function(user) {
 		if(user) {
