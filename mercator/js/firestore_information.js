81c906fbdb5929ad1e2d9668557597098695de0a
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-26 15:38:01 +0100

    added more code

--- mercator/js/firestore_information.js
+++ mercator/js/firestore_information.js
@@ -1,6 +1,21 @@
 window.onload = getProductInformation();
 
 function getProductInformation() {
-	let url_values = Object.values(getUrlVars());
-	console.log(url_values);
+	let ean = Object.values(getUrlVars())[0];
+	console.log(ean);
+	
+	let productRef = firebase.firestore().collection("mercator-product-review").doc(ean);
+	
+	let title = document.getElementsByTagName('h1')[0];
+	
+	productRef.get().then(function(doc) {
+		if (doc.exists) {
+			console.log("Document data:", doc.data());
+			title.innerHTML = doc.product_name;
+		} else {
+			console.log("No such document!");
+		}
+	}).catch(function(error) {
+		console.log("Error getting document:", error);
+	});
 }
