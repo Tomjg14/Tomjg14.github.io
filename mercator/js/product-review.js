32af5d417805bccaf50c7b11705b3bc704672f16
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-26 15:03:28 +0100

    changed small aspect of string

--- mercator/js/product-review.js
+++ mercator/js/product-review.js
@@ -31,6 +31,6 @@ async function getProducts() {
 		eanCell.innerHTML = docs[i].ean;
 		nameCell.innerHTML = docs[i].product_naam;
 		linkCell.innerHTML = "<a href='product-information.html'>Information</a>";
-		reviewCell.innerHTML = `${docs[i].positive_reviews} Yes // ${docs[i].negative_reviews} No`;
+		reviewCell.innerHTML = `${docs[i].positive_reviews} Yes / ${docs[i].negative_reviews} No`;
 	}
 }
