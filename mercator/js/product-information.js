f03cb9a1b80bcaececfb82e04d202de15f95532b
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-10-27 15:15:19 +0100

    added check for user log in

--- mercator/js/product-information.js
+++ mercator/js/product-information.js
@@ -1,4 +1,24 @@
 const backBtn = document.getElementById('back');
+const yesBtn = document.getElementById('yes');
+const noBtn = document.getElementById('no');
+
+yesBtn.addEventListener("click", e => {
+	e.preventDefault();
+	
+	var auth = firebase.auth()
+	auth.onAuthStateChanged(user => {
+		if (user) {
+			console.log(user.id);
+		} else {
+			console.log("no user");
+		}
+	});
+});
+
+noBtn.addEventListener("click", e => {
+	e.preventDefault();
+	
+});
 
 backBtn.addEventListener("click", e =>{
 	e.preventDefault();
