b5250c751380ab21b50f792ee8c5e652b7903943
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-11-01 14:27:08 +0100

    made sure to not show review buttons when already voted

--- mercator/js/product-information.js
+++ mercator/js/product-information.js
@@ -28,6 +28,9 @@ function userVoted() {
 				console.log(x);
 				if (x.includes(user.uid)) {
 					console.log("user already voted");
+					messageDiv.style.display = "block";
+					const reviewDiv = document.getElementById('review');
+					reviewDiv.style.display = "none";
 				} else {
 					console.log("user did not yet vote");
 				}
@@ -36,23 +39,6 @@ function userVoted() {
 			console.log("no user");
 		}
 	});
-}
-
-async function getVotes(ean,sign) {
-	let productRef = firebase.firestore().collection("mercator-product-review").doc(ean);
-	var votes = 0;
-	await productRef.get().then(function(doc) {
-		if (doc.exists) {
-			if (sign == "+") {
-				return doc.positive_reviews;
-			} else {
-				return doc.negative_reviews;
-			}
-		} else {
-			console.log("No such document!");
-		}
-	});
-	return votes
 }
 
 async function updateVotes(ean,sign) {
@@ -92,22 +78,7 @@ yesBtn.addEventListener("click", e => {
 noBtn.addEventListener("click", e => {
 	e.preventDefault();
 	let ean = Object.values(getUrlVars())[0];
-	let productRef = firebase.firestore().collection("mercator-product-review").doc(ean);
-	
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) {
-			console.log(user.uid);
-			votes = getVotes(ean,"-");
-			votes.then(function(x) {
-				productRef.update({
-					negative_reviews: x+1,
-					voted: firebase.firestore.FieldValue.arrayUnion(user.uid)
-				});
-			});
-		} else {
-			console.log("no user");
-		}
-	});
+	updateVotes(ean,"-");
 });
 
 backBtn.addEventListener("click", e =>{
