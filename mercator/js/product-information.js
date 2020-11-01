29eee72483d8e8e39b66f4a4512ed2df5147ec4f
Tom Janssen Groesbeek <tomjg@hotmail.nl>
2020-11-01 12:18:07 +0100

    tried something different

--- mercator/js/product-information.js
+++ mercator/js/product-information.js
@@ -8,7 +8,7 @@ async function getVoted(ean) {
 	let productRef = firebase.firestore().collection("mercator-product-review").doc(ean);
 	await productRef.get().then(function(doc) {
 		if (doc.exists) {
-			console.log(doc.voted);
+			console.log(doc.data());
 			return doc.voted;
 		} else {
 			console.log("No such document!");
